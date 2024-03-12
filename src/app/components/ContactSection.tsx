c326aa8cfdbbe0f08352b41e7003192ded337737
Pedro Henrique <pedroh.fss@gmail.com>
2024-03-12 16:26:22 -0300

    feat(contact): creation of the contact section

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -1,7 +1,18 @@
+import Form from "./Form";
+
 const ContactSection = () =>{
   return(
-    <section className="h-screen bg-tertiary">
-      <h1>Contact</h1>
+    <section className="h-full bg-tertiary px-48 py-32 flex flex-col justify-start gap-10">
+       <h1 className="font-mono text-secundary text-4xl">
+        Contact
+      </h1>
+      <div className="flex justify-evenly items-center mx-auto my-0 w-full h-full p-8">
+        <h2 className="text-5xl/normal w-[480px] h-auto font-mono font-bold text-primary-600">
+          Let&apos;s start a project together
+        </h2>
+        <Form />
+      </div>
+      
     </section>
   )
 }
---
+++ src/app/components/Form.tsx
@@ -0,0 +1,47 @@
+const Form = () =>{
+  return(
+    <form action="" className="flex flex-col gap-6 text-base text-white  w-[560px]">
+      <label htmlFor="name" className="flex gap-8">
+        <span>
+          01
+        </span>
+        What&apos;s your name?
+      </label>
+
+      <input type="text" className="bg-transparent border-b-2 border-tertiary-800 focus:outline-none focus:border-secundary transition-colors"/>
+
+      <label htmlFor="email" className="flex gap-8">
+        <span>
+          02
+        </span>
+        What&apos;s your email?
+      </label>
+
+      <input type="text" className="bg-transparent border-b-2 border-tertiary-800 focus:outline-none focus:border-secundary transition-colors" />
+
+      <label htmlFor="services" className="flex gap-8">
+        <span>
+          03
+        </span>
+          What services are you looking for?
+      </label>
+
+      <input type="text" className="bg-transparent border-b-2 border-tertiary-800 focus:outline-none focus:border-secundary transition-colors"/>
+
+      <label htmlFor="message" className="flex gap-8">
+        <span>
+          04
+        </span>
+        Your message
+      </label>
+
+      <textarea className="bg-transparent resize-y max-h-24 border-b-2 p-0 border-tertiary-800 focus:outline-none focus:border-secundary h-auto transition-colors" />
+
+      <button className="w-24 h-9 bg-secundary-800 relative left-3/4 text-sm rounded p-2 hover:bg-secundary hover:text-primary-900 focus:outline-none focus:ring-2 focus:ring-secundary-200">
+        Send it!
+      </button>
+    </form>
+  )
+}
+
+export default Form
